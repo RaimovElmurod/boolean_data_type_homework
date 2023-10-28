8a49a83016c9787cc2354e6a6b1988cd3620fb33
elmurod <elmurodraimov764@gmail.com>
2023-10-28 20:05:26 +0500

    code

--- bool04.py
+++ bool04.py
@@ -1,10 +1,4 @@
 def main(a):
-    """
-    check the following statement "The variable "a" is negative"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return  a < 0
+print(main(-3))
+print(main(8))
