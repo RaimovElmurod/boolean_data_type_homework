3fdfae4a3134342de48bf297a5644139e346cb20
elmurod <elmurodraimov764@gmail.com>
2023-10-28 20:29:29 +0500

    code

--- bool08.py
+++ bool08.py
@@ -1,10 +1,5 @@
 def main(a):
-    """
-    check the whole number. Integers are 0 and a positive number.
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return
+    return a>=0
+print(main(3))
+print(main(0))
+print(main(-1))
