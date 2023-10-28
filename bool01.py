c1bdf462943cad979df0af291f5dfbf1c98a3577
elmurod <elmurodraimov764@gmail.com>
2023-10-28 20:02:56 +0500

    code

--- bool01.py
+++ bool01.py
@@ -1,11 +1,4 @@
 def main(a,b):
-    """
-    Check the following statement "they are equal"
-    Args:
-        a: int
-        b: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return a == b
+print(main(1, 1))
+print(main(3, 1))
