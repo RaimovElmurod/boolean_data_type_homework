34556e436f95399eed7ae9d6081bf93f49c2c61b
elmurod <elmurodraimov764@gmail.com>
2023-10-29 18:31:01 +0500

    code

--- bool06.py
+++ bool06.py
@@ -1,10 +1,4 @@
 def main(a):
-    """
-    check the following statement "The variable "a" is an even number"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return a%2!=1
+print(main(8))
+print(main(5))
