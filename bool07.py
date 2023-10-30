006d351b8a187b22ebc14f3d9e55fb39251014a5
elmurod <elmurodraimov764@gmail.com>
2023-10-30 10:26:07 +0500

    code

--- bool07.py
+++ bool07.py
@@ -1,11 +1,4 @@
 def main(a,b):
-    """
-    check the following statement "They are not equal"
-    Args:
-        a: int
-        b: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return a!=b
+print(main(3,1))
+print(main(5,5))
