ca1b40b1bf10978ecb2bf0a54aded846e082d65f
elmurod <elmurodraimov764@gmail.com>
2023-10-30 10:30:29 +0500

    code

--- bool10.py
+++ bool10.py
@@ -1,10 +1,6 @@
+import math
 def main(a):
-    """
-    Check that the number "a" is a perfect square.
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return math.sqrt(a)%1==0
+print(main(9))
+print(main(15))
+print(main(121))
